3f9eaa187ba3c899ee6970b7b0b24e523fb9b603
Jindun Shao <jindun.shao@gmail.com>
2023-10-05 11:47:21 +0200

    Add daily challenge

--- prb.py
+++ prb.py
@@ -2407,3 +2407,54 @@ class Solution:
         def remove(self, key: int) -> None:
             if key in self.hashmap:
                 del self.hashmap[key]
+
+    # LC 229. Majority Element II (Medium)
+    # https://leetcode.com/problems/majority-element-ii/
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        # # One-pass with Counter - O(n)T, O(n)S
+        # th = len(nums) // 3  # threshhold
+        # counter = collections.Counter(nums)
+        # return [num for num, count in counter.items() if count > th]
+
+        # One-liner with Counter - O(n)T, O(n)S
+        # return [num for num, count in collections.Counter(nums).items() if count > len(nums) // 3]
+
+        # Boyer-Moore Voting Algorithm (double-pass cause len(res) <= 2) - O(n)T, O(1)S
+        # Counters for the potential majority elements
+        count1 = count2 = 0
+        # Potential majority element candidates
+        candidate1 = candidate2 = 0
+        # First pass to find potential majority elements.
+        for num in nums:
+            # If count1 is 0 and the current number is not equal to candidate2, update candidate1.
+            if count1 == 0 and num != candidate2:
+                count1 = 1
+                candidate1 = num
+            # If count2 is 0 and the current number is not equal to candidate1, update candidate2.
+            elif count2 == 0 and num != candidate1:
+                count2 = 1
+                candidate2 = num
+            # Update counts for candidate1 and candidate2.
+            elif candidate1 == num:
+                count1 += 1
+            elif candidate2 == num:
+                count2 += 1
+            # If the current number is different from both candidates, decrement their counts.
+            else:
+                count1 -= 1
+                count2 -= 1
+        result = []
+        threshold = len(nums) // 3  # Threshold for majority element
+        # Second pass to count occurrences of the potential majority elements.
+        count1 = count2 = 0
+        for num in nums:
+            if candidate1 == num:
+                count1 += 1
+            elif candidate2 == num:
+                count2 += 1
+        # Check if the counts of potential majority elements are greater than n/3 and add them to the result.
+        if count1 > threshold:
+            result.append(candidate1)
+        if count2 > threshold:
+            result.append(candidate2)
+        return result
