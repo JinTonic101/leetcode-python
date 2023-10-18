8e6f220f64056039357b99194f8286610304046a
Jindun Shao <jindun.shao@gmail.com>
2023-10-18 11:13:15 +0200

    Update daily challenge 2023-10-17

--- prb.py
+++ prb.py
@@ -2879,6 +2879,28 @@ class Solution:
         #             seen.add(child)
         # return len(seen) == n
 
+        class UnionFind:
+            def __init__(self, n):
+                self.components = n
+                self.parents = list(range(n))
+
+            def union(self, parent, child):
+                parent_parent = self.find(parent)
+                child_parent = self.find(child)
+
+                if child_parent != child or parent_parent == child_parent:
+                    return False
+
+                self.components -= 1
+                self.parents[child_parent] = parent_parent
+                return True
+
+            def find(self, node):
+                if self.parents[node] != node:
+                    self.parents[node] = self.find(self.parents[node])
+
+                return self.parents[node]
+
         # Union-Find - O(n)T, O(n)S
         uf = UnionFind(n)
         for node in range(n):
@@ -2888,25 +2910,3 @@ class Solution:
                 if not uf.union(node, child):
                     return False
         return uf.components == 1
-
-class UnionFind:
-    def __init__(self, n):
-        self.components = n
-        self.parents = list(range(n))
-
-    def union(self, parent, child):
-        parent_parent = self.find(parent)
-        child_parent = self.find(child)
-
-        if child_parent != child or parent_parent == child_parent:
-            return False
-
-        self.components -= 1
-        self.parents[child_parent] = parent_parent
-        return True
-
-    def find(self, node):
-        if self.parents[node] != node:
-            self.parents[node] = self.find(self.parents[node])
-
-        return self.parents[node]
